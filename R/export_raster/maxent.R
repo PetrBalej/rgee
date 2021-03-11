781c536e839c10d7ad5999ce717e8b9658bc72e3
Petr Balej <petr@balej.net>
2021-03-11 05:37:17 +0100

    Maxent - umožnění opakování

--- R/export_raster/maxent.R
+++ R/export_raster/maxent.R
@@ -38,7 +38,7 @@ wd <- paste0(path.expand("~"), "/Downloads/rgee2/rgee")
 
 setwd(wd)
 export_path <-
-  paste0(getwd(), "/../export/schuzka2-total-gbif-ndop/")
+  paste0(getwd(), "/../export/schuzka2-total-gbif-ndop3/")
 
 # pouze průnik NDOP a GBIF (totožné druhy odlišných názvů se nenapárují, Anas platyrhynchos+Turdus merula nelze kvůli vysokému počtu nálezů spočítat spThin)
 species <-
@@ -147,8 +147,8 @@ species <-
 
 
 species_u <- gsub(" ", "_", species)
-px_size <- c(100, 200, 1000) # 100, 200, 1000, 2000, 5000, 10000
-
+px_size <- c(2000) # 100, 200, 1000, 2000, 5000, 10000
+reps <- 10
 
 if (1 == 1) {
   me <- " pictures=FALSE outputGrids=FALSE plots=FALSE "
@@ -162,70 +162,117 @@ if (1 == 1) {
 # dataframe <- data.frame()
 for (px_size_item in px_size) {
   for (species_u_item in species_u) {
-    dir_key <- paste0(px_size_item, "_", species_u_item)
-    dir_key_ndop <- paste0(px_size_item, "_ndop_", species_u_item)
-    dir_key_gbif <- paste0(px_size_item, "_gbif_", species_u_item)
-    dir_key_all <- paste0(px_size_item, "_all_", species_u_item)
-    
-    
-    dir_key_1_p <- paste0(export_path, dir_key, "_1/")
-    dir_key_2_p <- paste0(export_path, dir_key, "_2/")
-    
-    #
-    # 1) vygenerování modelů
-    #
-    
-    # dir.create(dir_key_1_p, showWarnings = FALSE)
-    # dir.create(dir_key_2_p, showWarnings = FALSE)
-    # system(paste0("java -mx2000m -jar ~/maxent/test_project/maxent/maxent.jar nowarnings noprefixes  outputdirectory=", dir_key_1_p, " samplesfile=/home/petr/Downloads/rgee2/export/schuzka2-total-gbif-ndop/species/", dir_key_gbif, ".csv environmentallayers=/home/petr/Downloads/rgee2/export/raster/schuzka_rastery_vif/", px_size_item, " testsamplesfile=/home/petr/Downloads/rgee2/export/schuzka2-total-gbif-ndop/species/", dir_key_ndop, ".csv ", " randomtestpoints=0 replicates=0 writebackgroundpredictions responsecurvesexponent appendtoresultsfile threads=4  autorun visible=FALSE", me))
-    # system(paste0("java -mx2000m -jar ~/maxent/test_project/maxent/maxent.jar nowarnings noprefixes  outputdirectory=", dir_key_2_p, " samplesfile=/home/petr/Downloads/rgee2/export/schuzka2-total-gbif-ndop/species/", dir_key_all, ".csv environmentallayers=/home/petr/Downloads/rgee2/export/raster/schuzka_rastery_vif/", px_size_item, "  randomtestpoints=0 replicates=0 writebackgroundpredictions responsecurvesexponent appendtoresultsfile threads=4  autorun visible=FALSE", me))
-    
-    
-    #
-    # 2) načtení získaných statistik do CSV
-    #
-    
-    # csvgee_1 <- read_csv(paste0(dir_key_1_p, "maxentResults.csv"))
-    # csvgee_2 <- read_csv(paste0(dir_key_2_p, "maxentResults.csv"))
-    # csvgee_1 %<>% select(1:11)
-    # csvgee_2 %<>% select(2:7)
-    # csvgee_1$px_size_item <- px_size_item
-    # write_csv(bind_cols(csvgee_1[1,], csvgee_2[1,]), paste0(export_path, "results_100_200_1000-100top.csv"), append = TRUE)
-    
-    
-    ########################################
-    # csvgee_1$type <- 1
-    #csvgee_1$species <- species_u_item
-    
-    
-    # data.frame(type = 1, csvgee_1)
-    # data.frame(px_size_item = px_size_item, csvgee_1)
-    # data.frame(species = species_u_item, csvgee_1)
-    
-    # add_column(csvgee_1, type = 1, .before = 1)
-    # add_column(csvgee_1, px_size_item = px_size_item, .before = 1)
-    # add_column(csvgee_1, species = species_u_item, .before = 1)
-    
-    # csvgee_2$type <- 2
-    # csvgee_2$px_size_item <- px_size_item
-    # csvgee_2$species <- species_u_item
-    # data.frame(type = 1, csvgee_2)
-    # data.frame(px_size_item = px_size_item, csvgee_2)
-    # data.frame(species = species_u_item, csvgee_2)
-    
-    
-    # add_column(csvgee_2, type = 1, .before = 1)
-    # add_column(csvgee_2, px_size_item = px_size_item, .before = 1)
-    # add_column(csvgee_2, species = species_u_item, .before = 1)
-    
-    # csvgee_1$"Training.AUC"
-    # csvgee_1$"Test.AUC"
-    # csvgee_2$"Training.AUC"
-    
-    
-    # write_csv(csvgee_1[1,], paste0(export_path, "mv_1sT4.csv"), append = TRUE)
-    # write_csv(csvgee_2[1,], paste0(export_path, "mv_2sT4.csv"), append = TRUE)
-    ########################################
-    
+    for (i in 1:reps) {
+      dir_key <- paste0(px_size_item, "_", species_u_item)
+      
+      # ponechat změnu thinnování, nebo dát hodnotu napevno?
+      # px_size_item_change <- px_size_item
+      px_size_item_change <- 100
+      
+      dir_key_ndop <-
+        paste0(px_size_item_change, "_ndop_", species_u_item, "_", i)
+      dir_key_gbif <-
+        paste0(px_size_item_change, "_gbif_", species_u_item, "_", i)
+      dir_key_all <-
+        paste0(px_size_item_change, "_all_", species_u_item, "_", i)
+      
+      
+      dir_key_1_p <-
+        paste0(export_path, "maxent/", dir_key, "_1/", i, "/")
+      dir_key_2_p <-
+        paste0(export_path, "maxent/", dir_key, "_2/", i, "/")
+      
+      #
+      # 1) vygenerování modelů
+      #
+      
+      dir.create(dir_key_1_p,
+                 showWarnings = FALSE,
+                 recursive = TRUE)
+      dir.create(dir_key_2_p,
+                 showWarnings = FALSE,
+                 recursive = TRUE)
+      system(
+        paste0(
+          "java -mx2000m -jar ~/maxent/test_project/maxent/maxent.jar nowarnings noprefixes  outputdirectory=",
+          dir_key_1_p,
+          " samplesfile=",
+          export_path,
+          "species/",
+          dir_key_gbif,
+          ".csv environmentallayers=/home/petr/Downloads/rgee2/export/raster/schuzka_rastery_vif/",
+          px_size_item,
+          " testsamplesfile=",
+          export_path,
+          "species/",
+          dir_key_ndop,
+          ".csv ",
+          " randomtestpoints=0 replicates=0 writebackgroundpredictions responsecurvesexponent appendtoresultsfile threads=4  autorun visible=FALSE removeDuplicates=FALSE ",
+          me
+        )
+      )
+      system(
+        paste0(
+          "java -mx2000m -jar ~/maxent/test_project/maxent/maxent.jar nowarnings noprefixes  outputdirectory=",
+          dir_key_2_p,
+          " samplesfile=",
+          export_path,
+          "species/",
+          dir_key_all,
+          ".csv environmentallayers=/home/petr/Downloads/rgee2/export/raster/schuzka_rastery_vif/",
+          px_size_item,
+          "  randomtestpoints=0 replicates=0 writebackgroundpredictions responsecurvesexponent appendtoresultsfile threads=4  autorun visible=FALSE removeDuplicates=FALSE ",
+          me
+        )
+      )
+      
+      
+      #
+      # 2) načtení získaných statistik do CSV
+      #
+      
+      # csvgee_1 <- read_csv(paste0(dir_key_1_p, "maxentResults.csv"))
+      # csvgee_2 <- read_csv(paste0(dir_key_2_p, "maxentResults.csv"))
+      # csvgee_1 %<>% select(1:11)
+      # csvgee_2 %<>% select(2:7)
+      # csvgee_1$px_size_item <- px_size_item
+      # csvgee_1$repetition <- i
+      # write_csv(bind_cols(csvgee_1[1,], csvgee_2[1,]), paste0(export_path, "results_100_200_1000_2000-100top.csv"), append = TRUE)
+      
+      
+      ########################################
+      # csvgee_1$type <- 1
+      #csvgee_1$species <- species_u_item
+      
+      
+      # data.frame(type = 1, csvgee_1)
+      # data.frame(px_size_item = px_size_item, csvgee_1)
+      # data.frame(species = species_u_item, csvgee_1)
+      
+      # add_column(csvgee_1, type = 1, .before = 1)
+      # add_column(csvgee_1, px_size_item = px_size_item, .before = 1)
+      # add_column(csvgee_1, species = species_u_item, .before = 1)
+      
+      # csvgee_2$type <- 2
+      # csvgee_2$px_size_item <- px_size_item
+      # csvgee_2$species <- species_u_item
+      # data.frame(type = 1, csvgee_2)
+      # data.frame(px_size_item = px_size_item, csvgee_2)
+      # data.frame(species = species_u_item, csvgee_2)
+      
+      
+      # add_column(csvgee_2, type = 1, .before = 1)
+      # add_column(csvgee_2, px_size_item = px_size_item, .before = 1)
+      # add_column(csvgee_2, species = species_u_item, .before = 1)
+      
+      # csvgee_1$"Training.AUC"
+      # csvgee_1$"Test.AUC"
+      # csvgee_2$"Training.AUC"
+      
+      
+      # write_csv(csvgee_1[1,], paste0(export_path, "mv_1sT4.csv"), append = TRUE)
+      # write_csv(csvgee_2[1,], paste0(export_path, "mv_2sT4.csv"), append = TRUE)
+      ########################################
+    }
   }
 }
