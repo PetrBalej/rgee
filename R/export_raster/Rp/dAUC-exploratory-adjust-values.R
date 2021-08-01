274df7d767f4159bd11e09b6045f7629489e12a8
Petr Balej <petr@balej.net>
2021-08-01 03:58:01 +0200

    načtení všech  3 datasetů

--- R/export_raster/Rp/dAUC-exploratory-adjust-values.R
+++ R/export_raster/Rp/dAUC-exploratory-adjust-values.R
@@ -13,57 +13,63 @@ setwd(wd)
 
 export_path <- "/mnt/2AA56BAE3BB1EC2E/Downloads/rgee2/pdf_statsIGA"
 
-# výběr datasetu
-prefix <- "glm_fmt_ndop_" # _gbif_ _all_
+datasets <- c("ndop", "all", "gbif")
 
-rds_list <-
-  list.files(
-    path = "/mnt/2AA56BAE3BB1EC2E/Downloads/rgee2/prelim/occurrences", # vse-v-jesdnom/outputs/rds/
-    pattern = paste0("^", prefix, ".*\\.rds$"), # "^enmsr_glmE[0-9]_.+\\.rds$"  "^enmsr_xxx[0-9]_.+\\.rds$"   enmsr_glmF0_5000_3_1621355712.12381.rds       "^enmsr_glm2PATEST[0-9]_.+\\.rds$"
-    ignore.case = TRUE,
-    full.names = TRUE
-  )
-
-
-rds_append <- list()
-
-rds <- readRDS(rds_list[[1]])
-rds_names <- names(rds)
-rds_append[[rds_names]] <- rds[[rds_names]]
+tibbleT <- list()
+for (d in datasets) {
+  rds_list <-
+    list.files(
+      path = "/mnt/2AA56BAE3BB1EC2E/Downloads/rgee2/prelim/occurrences", # vse-v-jesdnom/outputs/rds/
+      pattern = paste0("^glm_fmt_", d, "_.*\\.rds$"), # "^enmsr_glmE[0-9]_.+\\.rds$"  "^enmsr_xxx[0-9]_.+\\.rds$"   enmsr_glmF0_5000_3_1621355712.12381.rds       "^enmsr_glm2PATEST[0-9]_.+\\.rds$"
+      ignore.case = TRUE,
+      full.names = TRUE
+    )
 
 
-rds_list <- rds_list[-1]
-for (i in seq_along(rds_list)) {
-  rds <- readRDS(rds_list[[i]])
+  rds_append <- list()
+
+  rds <- readRDS(rds_list[[1]])
   rds_names <- names(rds)
+  rds_append[[rds_names]] <- rds[[rds_names]]
 
-  rds_append[[rds_names]] <- append(rds_append[[rds_names]], rds[[rds_names]])
-  # rds_append <- append(rds_append, readRDS(rds_list[[i]]))
+
+  rds_list <- rds_list[-1]
+  for (i in seq_along(rds_list)) {
+    rds <- readRDS(rds_list[[i]])
+    rds_names <- names(rds)
+
+    rds_append[[rds_names]] <- append(rds_append[[rds_names]], rds[[rds_names]])
+    # rds_append <- append(rds_append, readRDS(rds_list[[i]]))
+  }
+
+  tibble <- list()
+  for (i in names(rds_append)) {
+    print("-----------------------------------------------------------------------------------------------------")
+    print(i)
+    tibble[[i]] <- rds_append[[i]] %>% # ttemp
+      map_depth(1, na.omit) %>%
+      map(as_tibble) %>%
+      bind_rows(.id = "species") %>%
+      mutate(px_size = i)
+    # # %>%
+    # # #group_by(species) %>%
+    # distinct(species, .keep_all = TRUE)
+
+    #      print(tibble)
+    # tibble %<>% tibble %>% bind_rows(.id = "species")
+
+    # tibble %<>% tibble#  %>% group_by(species, px_size) # sp = paste(species, px_size_item)
+  }
+
+  tibbleT[[d]] <- bind_rows(tibble) %>% mutate(dataset = d)
 }
 
-tibble <- list()
-for (i in names(rds_append)) {
-  print("-----------------------------------------------------------------------------------------------------")
-  print(i)
-  tibble[[i]] <- rds_append[[i]] %>% # ttemp
-    map_depth(1, na.omit) %>%
-    map(as_tibble) %>%
-    bind_rows(.id = "species") %>%
-    mutate(px_size = i)
-  # # %>%
-  # # #group_by(species) %>%
-  # distinct(species, .keep_all = TRUE)
+tibbleB <- bind_rows(tibbleT)
 
-  #      print(tibble)
-  # tibble %<>% tibble %>% bind_rows(.id = "species")
-
-  # tibble %<>% tibble#  %>% group_by(species, px_size) # sp = paste(species, px_size_item)
-}
-
-tibbleB <- bind_rows(tibble)
 tibbleB$px_size %<>% as.integer
 tibbleB$species %<>% as.factor
-tibbleB %<>% group_by(species, px_size)
+tibbleB$dataset %<>% as.factor
+tibbleB %<>% group_by(species, px_size, dataset)
 
 
 # # následující dává smysl jen v případě vybrané JEDNÉ ideální hodnoty adjustu
