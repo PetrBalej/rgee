0576c2ed965cf0cd566ad1c6bcbff0f9954ca526
Petr Balej <petr@balej.net>
2022-03-05 05:51:58 +0100

    multiply metriky - doplnění AUC

--- R/export_raster/multiply-metrics-selection-cycle.R
+++ R/export_raster/multiply-metrics-selection-cycle.R
@@ -14,8 +14,8 @@ setwd(wd)
 # export_path <- "G:/balej/iga/vse-v-jednom"
 export_path <- "/mnt/2AA56BAE3BB1EC2E/Downloads/rgee2/vse-v-jednom/UN"
 
-df <- data.frame(matrix(ncol = 5, nrow = 0))
-colnames(df) <- c("species", "px", "pxv", "md", "md.max")
+df <- data.frame(matrix(ncol = 6, nrow = 0))
+colnames(df) <- c("species", "px", "pxv", "md", "md.max", "ndop.auc")
 
 for (px in c(500, 1000, 2000, 5000, 10000)) {
     px <- px
@@ -25,6 +25,17 @@ for (px in c(500, 1000, 2000, 5000, 10000)) {
             next
         }
         pxv <- pxv
+
+        rds <-
+            list.files(
+                path = paste0(export_path, "/outputs/rds"),
+                # původní: "^glm_fmt_", d, "_.*\\.rds$"
+                # "^glm_fmt_", d, "_.*[0-9]{4,5}-[0-9]{1,2}\\.rds$" - vše bez 500
+                pattern = paste0("^enmsr_glm_UN_", px, pxv, "_", px, "_4_[0-9]+_scottIso-adj-0.65\\.rds$"),
+                ignore.case = TRUE,
+                full.names = TRUE
+            )
+        ndopCheckerboard2 <- readRDS(rds)
 
         n8.D <- readRDS(paste0(export_path, "/outputs/fitting/glm_fmt_gbif.D_", px, "-", pxv, ".rds"))
         n8.rmse <- readRDS(paste0(export_path, "/outputs/fitting/glm_fmt_gbif.rmse_", px, "-", pxv, ".rds"))
@@ -36,6 +47,8 @@ for (px in c(500, 1000, 2000, 5000, 10000)) {
             pv8.D <- n8.D[[as.character(px)]][[as.character(name)]]
             pv8.cor <- n8.cor[[as.character(px)]][[as.character(name)]]
             pv8.rmse <- n8.rmse[[as.character(px)]][[as.character(name)]]
+
+            ndopCheckerboard2.auc <- ndopCheckerboard2[[as.character(px)]][[as.character(name)]]$auc.ndop.te
 
             pv8multiply <- pv8.D * pv8.cor * (1 - pv8.rmse)
 
@@ -53,7 +66,7 @@ for (px in c(500, 1000, 2000, 5000, 10000)) {
             # )
             # points(md.max, pch = 19, col = "red")
 
-            df[nrow(df) + 1, ] <- c(name, px, pxv, md.max$nms, round(md.max$V2, 4))
+            df[nrow(df) + 1, ] <- c(name, px, pxv, md.max$nms, round(md.max$V2, 4), round(ndopCheckerboard2.auc, 4))
             # select treshold: 0.7*0.8*0.8=0.448
         }
     }
@@ -61,73 +74,80 @@ for (px in c(500, 1000, 2000, 5000, 10000)) {
 
 
 df <- as_tibble(df) %>% type_convert(
-    col_types = cols(species = "f", px = "f", md = "d", md.max = "d")
+    col_types = cols(species = "f", px = "f", md = "d", md.max = "d", ndop.auc = "d")
 )
+stop()
 
+auc.min <- 0.7
 # souhrny
-summary(df %>% filter(px == 2000) %>% (md.max > 0.45))
-summary(df %>% filter(md.max > 0.45))
+summary(df %>% filter(ndop.auc > auc.min) %>% filter(px == 2000) %>% filter(md.max > 0.45))
+summary(df %>% filter(ndop.auc > auc.min) %>% filter(md.max > 0.45))
 
 df %>%
     group_by(species) %>%
+    filter(ndop.auc > auc.min) %>%
+    # filter(px == 10000) %>%
     filter(md.max > 0.45)
-unique(df %>% filter(md.max > 0.45) %>% select(species))
+unique(df %>% filter(ndop.auc > auc.min) %>% filter(md.max > 0.45) %>% select(species))
 
 # počet unikátních druhů, které nepořebovaly bias korekci! (kernel width 0 ): 3 druhy, jeden 3 rozlišení
 df %>%
     group_by(species) %>%
+    filter(ndop.auc > auc.min) %>%
     filter(md == 0.00) %>%
     filter(md.max > 0.45)
-unique(df %>% filter(md == 0.00) %>% filter(md.max > 0.45) %>% select(species))
+unique(df %>% filter(ndop.auc > auc.min) %>% filter(md == 0.00) %>% filter(md.max > 0.45) %>% select(species))
 
-# # A tibble: 5 x 5
+# # A tibble: 5 x 6
 # # Groups:   species [3]
-#   species               px      pxv    md md.max
-#   <fct>                 <fct> <dbl> <dbl>  <dbl>
-# 1 Himantopus himantopus 500       5     0  0.548
-# 2 Limosa limosa         500       6     0  0.541
-# 3 Himantopus himantopus 2000      5     0  0.598
-# 4 Himantopus himantopus 10000     5     0  0.472
-# 5 Merops apiaster       10000     7     0  0.506
+#   species               px      pxv    md md.max ndop.auc
+#   <fct>                 <fct> <dbl> <dbl>  <dbl>    <dbl>
+# 1 Himantopus himantopus 500       5     0  0.548    0.941
+# 2 Limosa limosa         500       6     0  0.541    0.905
+# 3 Himantopus himantopus 2000      5     0  0.598    0.950
+# 4 Himantopus himantopus 10000     5     0  0.472    0.901
+# 5 Merops apiaster       10000     7     0  0.506    0.858
 
 
 # nebo jen malá bias korekce
 df %>%
     group_by(species) %>%
+    filter(ndop.auc > auc.min) %>%
     filter(md <= 0.20) %>%
     filter(md.max > 0.45)
-unique(df %>% filter(md <= 0.20) %>% filter(md.max > 0.45) %>% select(species))
+unique(df %>% filter(ndop.auc > auc.min) %>% filter(md <= 0.20) %>% filter(md.max > 0.45) %>% select(species))
 
 
+# # A tibble: 17 x 6
+# # Groups:   species [10]
+#    species                 px      pxv    md md.max ndop.auc
+#    <fct>                   <fct> <dbl> <dbl>  <dbl>    <dbl>
+#  1 Himantopus himantopus   500       5  0     0.548    0.941
+#  2 Limosa limosa           500       6  0     0.541    0.905
+#  3 Himantopus himantopus   1000      5  0.2   0.590    0.963
+#  4 Himantopus himantopus   2000      5  0     0.598    0.950
+#  5 Limosa limosa           2000      6  0.2   0.552    0.945
+#  6 Botaurus stellaris      5000      2  0.2   0.481    0.795
+#  7 Dendrocoptes medius     5000      4  0.2   0.474    0.760
+#  8 Himantopus himantopus   5000      5  0.2   0.598    0.930
+#  9 Limosa limosa           5000      6  0.2   0.522    0.842
+# 10 Merops apiaster         5000      7  0.2   0.624    0.881
+# 11 Netta rufina            5000      7  0.2   0.502    0.843
+# 12 Panurus biarmicus       5000      7  0.2   0.545    0.862
+# 13 Recurvirostra avosetta  5000      8  0.2   0.491    0.871
+# 14 Crex crex               10000     4  0.15  0.510    0.784
+# 15 Himantopus himantopus   10000     5  0     0.472    0.901
+# 16 Merops apiaster         10000     7  0     0.506    0.858
+# 17 Nucifraga caryocatactes 10000     7  0.2   0.550    0.753
 
-df45 <- df %>% filter(md.max > 0.45)
+
+df45 <- df %>%
+    filter(ndop.auc > auc.min) %>%
+    filter(md.max > 0.45)
+
 hist(df45$md)
 
 hist(df$md.max)
-
-# # A tibble: 19 x 5
-# # Groups:   species [12]
-#    species                 px      pxv    md md.max
-#    <fct>                   <fct> <dbl> <dbl>  <dbl>
-#  1 Himantopus himantopus   500       5  0     0.548
-#  2 Limosa limosa           500       6  0     0.541
-#  3 Himantopus himantopus   1000      5  0.2   0.590
-#  4 Himantopus himantopus   2000      5  0     0.598
-#  5 Limosa limosa           2000      6  0.2   0.552
-#  6 Botaurus stellaris      5000      2  0.2   0.481
-#  7 Dendrocoptes medius     5000      4  0.2   0.474
-#  8 Himantopus himantopus   5000      5  0.2   0.598
-#  9 Limosa limosa           5000      6  0.2   0.522
-# 10 Merops apiaster         5000      7  0.2   0.624
-# 11 Netta rufina            5000      7  0.2   0.502
-# 12 Panurus biarmicus       5000      7  0.2   0.545
-# 13 Recurvirostra avosetta  5000      8  0.2   0.491
-# 14 Cinclus cinclus         10000     3  0.15  0.465
-# 15 Crex crex               10000     4  0.15  0.510
-# 16 Himantopus himantopus   10000     5  0     0.472
-# 17 Merops apiaster         10000     7  0     0.506
-# 18 Nucifraga caryocatactes 10000     7  0.2   0.550
-# 19 Spinus spinus           10000     9  0.15  0.452
 
 
 # par(new = TRUE
@@ -136,17 +156,3 @@ hist(df$md.max)
 # plot(pv8.cor, col = "darkgreen")
 # par(new = TRUE)
 # plot(pv8.rmse$nms, 1 - pv8.rmse$V2, col = "blue")
-
-
-
-# pv8.D <- n8.D$`2000`$`Podiceps cristatus`
-# pv8.cor <- n8.cor$`2000`$`Podiceps cristatus`
-# pv8.rmse <- n8.rmse$`2000`$`Podiceps cristatus`
-
-# pv8.D <- n8.D$`2000`$`Prunella modularis`
-# pv8.cor <- n8.cor$`2000`$`Prunella modularis`
-# pv8.rmse <- n8.rmse$`2000`$`Prunella modularis`
-
-# pv8.D <- n8.D$`2000`$`Phylloscopus collybita`
-# pv8.cor <- n8.cor$`2000`$`Phylloscopus collybita`
-# pv8.rmse <- n8.rmse$`2000`$`Phylloscopus collybita`
