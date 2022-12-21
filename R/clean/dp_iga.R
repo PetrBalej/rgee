e992dc15d1abff824b0727d97554121b75b385b1
Petr Balej <petr@balej.net>
2022-12-21 17:40:51 +0100

    redukce na vybrané kvadráty

--- R/clean/dp_iga.R
+++ R/clean/dp_iga.R
@@ -52,7 +52,7 @@ tag_name <- scale # "" # gsub('[^0-9-]', '-', Sys.time())
 
 # adresář pro exportované soubory (v rámci wd) + další tag_name
 export_path <- paste0(path.igaD, "predsDP/")
-export_fileName <- "kfme0rad-czechia_wc_l8_2014-2017_4-8"
+export_fileName <- "0rad-czechia_wc_l8_2014-2017_4-8_mean"
 
 
 # GIT project directory (kompletní repozitář rgee z github.com: po rozbalení zipu v rgee-master/rgee-master)
@@ -60,6 +60,11 @@ git_project_path <- getwd()
 
 
 ## výběr regionu
+
+SitMap.selected <- unname(unlist(read_csv(paste0(wd, "/R/clean/SitMap_0Rad-selected.csv"))))
+SitMap <- st_transform(st_read(paste0(path.igaD, "sitmap_0rad/sitmap_0rad.shp")), crs = 4326) %>% dplyr::select(POLE)
+SitMap.POLE.selected <- SitMap %>% dplyr::filter(POLE %in% SitMap.selected)
+
 
 # definice obálek (bounding box) různě velkých území pro testování
 sz_cechy <- list(
@@ -144,245 +149,233 @@ source(paste0(git_project_path, "/R/export_raster/functions.R"))
 # načtení csv s datasety z GEE
 gdl <- gee_datasets_list(gee_datasets_path_csv)
 
-if (!is.character(bb)) {
-    xmin <- bb$xmin
-    xmax <- bb$xmax
-    ymin <- bb$ymin
-    ymax <- bb$ymax
 
-    bb_geometry <- NULL
-    bb_geometry_rectangle <- ee$Geometry$Rectangle(
-        coords = c(xmin, ymin, xmax, ymax),
-        proj = "EPSG:4326",
-        geodesic = FALSE
-    )
-} else {
-    if (file.exists(bb)) {
-        bb_geometry_ee <- st_read(bb) %>% sf_as_ee()
-        bb_geometry <- bb_geometry_ee$geometry() # [[1]]$getInfo()
-        bb_geometry_rectangle <- bb_geometry_ee$geometry()$bounds()
-    } else {
-        stop(paste0("Shapefile ", bb, " not exist!"))
-    }
-}
+xmin <- bb$xmin
+xmax <- bb$xmax
+ymin <- bb$ymin
+ymax <- bb$ymax
 
+bb_geometry <- NULL
+bb_geometry_rectangle <- ee$Geometry$Rectangle(
+    coords = c(xmin, ymin, xmax, ymax),
+    proj = "EPSG:4326",
+    geodesic = FALSE
+)
 
-sitmap_2rad <- list()
-# Define a geometry
-sitmap_2rad$all <- st_transform(st_read(paste0(path.igaD, "sitmap_0rad/sitmap_0rad.shp")), crs = 4326) %>% dplyr::select(POLE)
+POLE <- SitMap.POLE.selected %>% sf_as_ee()
+envelope <- st_as_sfc(st_bbox(SitMap.POLE.selected)) %>% sf_as_ee()
 
-# sitmap_2rad$N <- st_read(paste0(path.igaD, "sitmap_2rad/sitmap_2rad_4326_czechia_N.shp"))
-# sitmap_2rad$S <- st_read(paste0(path.igaD, "sitmap_2rad/sitmap_2rad_4326_czechia_S.shp"))
 output_df <- list()
 
 start_time <- Sys.time()
-for (half in names(sitmap_2rad)) {
-    for (season in season_months_range) {
 
+for (season in season_months_range) {
+    print("season:")
+    print(season[1])
 
-        ################################################################
-        # L8 _SR 'LANDSAT/LC08/C01/T1_SR' - raw bandy
-        ################################################################
+    ################################################################
+    # L8 _SR 'LANDSAT/LC08/C01/T1_SR' - raw bandy
+    ################################################################
 
-        # aplikace základních geografických, časových a odmračňovacích/odstiňovacích/aerosol/radio filtrů
-        l8_sr_collection <-
-            ee$ImageCollection(gdl$landsat2$geeSnippet)$
-                # filterBounds(bb_geometry_rectangle)$ # nevhodné dělat předem?
-                filterDate(years_range$from, years_range$to)$
-                filter(
-                ee$Filter$calendarRange(season[1], season[2], "month")
-            )$map(mask_L8_sr2_qa)$map(mask_L8_sr2_radsat)$map(mask_L8_sr2_aerosol)$map(applyScaleFactors)
-
-
-
-
-
-        bands_all <- c("B1", "B2", "B3", "B4", "B5", "B6", "B7")
-        bn <- l8_sr_collection$first()$bandNames()$getInfo()
-        bn.renamed <- gsub("SR_|ST_", "", bn)
-
-        l8_sr_collection <- l8_sr_collection$select(bn, bn.renamed)
-
-        #####
-        # raw
-        #####
-        output_df[[paste0("l8_", scale, "_", season[1], "_raw_stdev")]] <- ee_extract(
-            x = l8_sr_collection$select(bands_all)$mean(),
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$stdDev(),
-            sf = FALSE
-        )
-
-        output_df[[paste0("l8_", scale, "_", season[1], "_raw_mean")]] <- ee_extract(
-            x = l8_sr_collection$select(bands_all)$mean(),
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$mean(),
-            sf = FALSE
-        )
-
-        #####
-        # ndvi
-        #####
-        output_df[[paste0("l8_", scale, "_", season[1], "_ndvi_stdev")]] <- ee_extract(
-            x = l8_sr_collection$select(c("B5", "B4"))$mean()$normalizedDifference(c("B5", "B4")),
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$stdDev(),
-            sf = FALSE
-        )
-
-        output_df[[paste0("l8_", scale, "_", season[1], "_ndvi_mean")]] <- ee_extract(
-            x = l8_sr_collection$select(c("B5", "B4"))$mean()$normalizedDifference(c("B5", "B4")),
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$mean(),
-            sf = FALSE
-        )
-
-
-        #####
-        # mndwi
-        #####
-        # https://www.linkedin.com/pulse/ndvi-ndbi-ndwi-calculation-using-landsat-7-8-tek-bahadur-kshetri
-        output_df[[paste0("l8_", scale, "_", season[1], "_mndwi_stdev")]] <- ee_extract(
-            x = l8_sr_collection$select(c("B3", "B6"))$mean()$normalizedDifference(c("B3", "B6")),
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$stdDev(),
-            sf = FALSE
-        )
-
-        output_df[[paste0("l8_", scale, "_", season[1], "_mndwi_mean")]] <- ee_extract(
-            x = l8_sr_collection$select(c("B3", "B6"))$mean()$normalizedDifference(c("B3", "B6")),
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$mean(),
-            sf = FALSE
-        )
-
-
-        #####
-        # ndwi (new)
-        #####
-        # https://www.linkedin.com/pulse/ndvi-ndbi-ndwi-calculation-using-landsat-7-8-tek-bahadur-kshetri
-        # NDWI = (NIR – SWIR) / (NIR + SWIR)
-        output_df[[paste0("l8_", scale, "_", season[1], "_ndwi_stdev")]] <- ee_extract(
-            x = l8_sr_collection$select(c("B5", "B6"))$mean()$normalizedDifference(c("B5", "B6")),
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$stdDev(),
-            sf = FALSE
-        )
-
-        output_df[[paste0("l8_", scale, "_", season[1], "_ndwi_mean")]] <- ee_extract(
-            x = l8_sr_collection$select(c("B5", "B6"))$mean()$normalizedDifference(c("B5", "B6")),
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$mean(),
-            sf = FALSE
-        )
-
-        #####
-        # evi
-        #####
-        #   # https://www.usgs.gov/core-science-systems/nli/landsat/landsat-enhanced-vegetation-index?qt-science_support_page_related_con=0
-        #   # In Landsat 8, EVI = 2.5 * ((Band 5 – Band 4) / (Band 5 + 6 * Band 4 – 7.5 * Band 2 + 1)).
-
-        evi_prep <- l8_sr_collection$select(c("B5", "B4", "B2"))$mean()
-
-        evi_res <- evi_prep$expression(
-            "2.5 * ((B5 - B4) / (B5 + 6 * B4 - 7.5 * B2 + 1))",
-            list(
-                B5 = evi_prep$select("B5"),
-                B4 = evi_prep$select("B4"),
-                B2 = evi_prep$select("B2")
-            )
-        )
-
-        output_df[[paste0("l8_", scale, "_", season[1], "_evi_stdev")]] <- ee_extract(
-            x = evi_res,
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$stdDev(),
-            sf = FALSE
-        )
-
-        output_df[[paste0("l8_", scale, "_", season[1], "_evi_mean")]] <- ee_extract(
-            x = evi_res,
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$mean(),
-            sf = FALSE
-        )
-
-        #####
-        # msavi
-        #####
-        #   # https://www.usgs.gov/core-science-systems/nli/landsat/landsat-modified-soil-adjusted-vegetation-index
-        #   # In Landsat 8, MSAVI = (2 * Band 5 + 1 – sqrt ((2 * Band 5 + 1)2 – 8 * (Band 5 – Band 4))) / 2.
-
-        msavi_prep <- l8_sr_collection$select(c("B5", "B4"))$mean()
-        msavi_res <- msavi_prep$expression(
-            "(2 * B5 + 1 - sqrt(pow((2 * B5 + 1), 2) - 8 * (B5 - B4))) / 2",
-            list(
-                B5 = msavi_prep$select("B5"),
-                B4 = msavi_prep$select("B4")
-            )
-        )
-
-        output_df[[paste0("l8_", scale, "_", season[1], "_msavi_stdev")]] <- ee_extract(
-            x = msavi_res,
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$stdDev(),
-            sf = FALSE
-        )
-
-        output_df[[paste0("l8_", scale, "_", season[1], "_msavi_mean")]] <- ee_extract(
-            x = msavi_res,
-            y = sitmap_2rad[[half]],
-            scale = scale,
-            fun = ee$Reducer$mean(),
-            sf = FALSE
-        )
-    }
-
-
-    # ################################################################
-    # # Worldclim/Bioclim 'WORLDCLIM/V1/BIO'
-    # ################################################################
-
-    wc <- ee$Image(gdl$worldclim$geeSnippet)
-
-    bands_all <- wc$bandNames()$getInfo()
-    # bands_all <- c("bio01", "bio02")
+    # aplikace základních geografických, časových a odmračňovacích/odstiňovacích/aerosol/radio filtrů
+    l8_sr_collection <-
+        ee$ImageCollection(gdl$landsat2$geeSnippet)$
+            # filterBounds(bb_geometry_rectangle)$ # nevhodné dělat předem?
+            filterDate(years_range$from, years_range$to)$
+            filter(
+            ee$Filter$calendarRange(season[1], season[2], "month")
+        )$map(mask_L8_sr2_qa)$map(mask_L8_sr2_radsat)$map(mask_L8_sr2_aerosol)$map(applyScaleFactors)
 
 
 
-    # Extract values
-    output_df[[paste0("wc_", scale, "_", season[1], "_stdev")]] <- ee_extract(
-        x = wc,
-        y = sitmap_2rad[[half]],
+
+
+    bands_all <- c("B1", "B2", "B3", "B4", "B5", "B6", "B7")
+    bn <- l8_sr_collection$first()$bandNames()$getInfo()
+    bn.renamed <- gsub("SR_|ST_", "", bn)
+
+    l8_sr_collection <- l8_sr_collection$select(bn, bn.renamed)
+
+    #####
+    # raw
+    #####
+    output_df[[paste0("l8_", scale, "_", season[1], "_raw_stdev")]] <- ee_extract(
+        x = l8_sr_collection$select(bands_all)$mean(),
+        y = SitMap.POLE.selected,
         scale = scale,
         fun = ee$Reducer$stdDev(),
         sf = FALSE
     )
 
-    output_df[[paste0("wc_", scale, "_", season[1], "_mean")]] <- ee_extract(
-        x = wc,
-        y = sitmap_2rad[[half]],
+    output_df[[paste0("l8_", scale, "_", season[1], "_raw_mean")]] <- ee_extract(
+        x = l8_sr_collection$select(bands_all)$mean(),
+        y = SitMap.POLE.selected,
+        scale = scale,
+        fun = ee$Reducer$mean(),
+        sf = FALSE
+    )
+
+    #####
+    # ndvi
+    #####
+    output_df[[paste0("l8_", scale, "_", season[1], "_ndvi_stdev")]] <- ee_extract(
+        x = l8_sr_collection$select(c("B5", "B4"))$mean()$normalizedDifference(c("B5", "B4")),
+        y = SitMap.POLE.selected,
+        scale = scale,
+        fun = ee$Reducer$stdDev(),
+        sf = FALSE
+    )
+
+    output_df[[paste0("l8_", scale, "_", season[1], "_ndvi_mean")]] <- ee_extract(
+        x = l8_sr_collection$select(c("B5", "B4"))$mean()$normalizedDifference(c("B5", "B4")),
+        y = SitMap.POLE.selected,
         scale = scale,
         fun = ee$Reducer$mean(),
         sf = FALSE
     )
 
 
+    #####
+    # mndwi
+    #####
+    # https://www.linkedin.com/pulse/ndvi-ndbi-ndwi-calculation-using-landsat-7-8-tek-bahadur-kshetri
+    output_df[[paste0("l8_", scale, "_", season[1], "_mndwi_stdev")]] <- ee_extract(
+        x = l8_sr_collection$select(c("B3", "B6"))$mean()$normalizedDifference(c("B3", "B6")),
+        y = SitMap.POLE.selected,
+        scale = scale,
+        fun = ee$Reducer$stdDev(),
+        sf = FALSE
+    )
 
-    # saveRDS(output_df, file = paste0(path.igaD, "test-kfme16-N_czechia_wc_l8_2018-2021_4-6.rds"))
-    saveRDS(output_df, file = paste0(export_path, export_fileName, "---", half, ".rds"))
+    output_df[[paste0("l8_", scale, "_", season[1], "_mndwi_mean")]] <- ee_extract(
+        x = l8_sr_collection$select(c("B3", "B6"))$mean()$normalizedDifference(c("B3", "B6")),
+        y = SitMap.POLE.selected,
+        scale = scale,
+        fun = ee$Reducer$mean(),
+        sf = FALSE
+    )
+
+
+    #####
+    # ndwi (new)
+    #####
+    # https://www.linkedin.com/pulse/ndvi-ndbi-ndwi-calculation-using-landsat-7-8-tek-bahadur-kshetri
+    # NDWI = (NIR – SWIR) / (NIR + SWIR)
+    output_df[[paste0("l8_", scale, "_", season[1], "_ndwi_stdev")]] <- ee_extract(
+        x = l8_sr_collection$select(c("B5", "B6"))$mean()$normalizedDifference(c("B5", "B6")),
+        y = SitMap.POLE.selected,
+        scale = scale,
+        fun = ee$Reducer$stdDev(),
+        sf = FALSE
+    )
+
+    output_df[[paste0("l8_", scale, "_", season[1], "_ndwi_mean")]] <- ee_extract(
+        x = l8_sr_collection$select(c("B5", "B6"))$mean()$normalizedDifference(c("B5", "B6")),
+        y = SitMap.POLE.selected,
+        scale = scale,
+        fun = ee$Reducer$mean(),
+        sf = FALSE
+    )
+
+    #####
+    # evi
+    #####
+    #   # https://www.usgs.gov/core-science-systems/nli/landsat/landsat-enhanced-vegetation-index?qt-science_support_page_related_con=0
+    #   # In Landsat 8, EVI = 2.5 * ((Band 5 – Band 4) / (Band 5 + 6 * Band 4 – 7.5 * Band 2 + 1)).
+
+    evi_prep <- l8_sr_collection$select(c("B5", "B4", "B2"))$mean()
+
+    evi_res <- evi_prep$expression(
+        "2.5 * ((B5 - B4) / (B5 + 6 * B4 - 7.5 * B2 + 1))",
+        list(
+            B5 = evi_prep$select("B5"),
+            B4 = evi_prep$select("B4"),
+            B2 = evi_prep$select("B2")
+        )
+    )
+
+    output_df[[paste0("l8_", scale, "_", season[1], "_evi_stdev")]] <- ee_extract(
+        x = evi_res,
+        y = SitMap.POLE.selected,
+        scale = scale,
+        fun = ee$Reducer$stdDev(),
+        sf = FALSE
+    )
+
+    output_df[[paste0("l8_", scale, "_", season[1], "_evi_mean")]] <- ee_extract(
+        x = evi_res,
+        y = SitMap.POLE.selected,
+        scale = scale,
+        fun = ee$Reducer$mean(),
+        sf = FALSE
+    )
+
+    #####
+    # msavi
+    #####
+    #   # https://www.usgs.gov/core-science-systems/nli/landsat/landsat-modified-soil-adjusted-vegetation-index
+    #   # In Landsat 8, MSAVI = (2 * Band 5 + 1 – sqrt ((2 * Band 5 + 1)2 – 8 * (Band 5 – Band 4))) / 2.
+
+    msavi_prep <- l8_sr_collection$select(c("B5", "B4"))$mean()
+    msavi_res <- msavi_prep$expression(
+        "(2 * B5 + 1 - sqrt(pow((2 * B5 + 1), 2) - 8 * (B5 - B4))) / 2",
+        list(
+            B5 = msavi_prep$select("B5"),
+            B4 = msavi_prep$select("B4")
+        )
+    )
+
+    output_df[[paste0("l8_", scale, "_", season[1], "_msavi_stdev")]] <- ee_extract(
+        x = msavi_res,
+        y = SitMap.POLE.selected,
+        scale = scale,
+        fun = ee$Reducer$stdDev(),
+        sf = FALSE
+    )
+
+    output_df[[paste0("l8_", scale, "_", season[1], "_msavi_mean")]] <- ee_extract(
+        x = msavi_res,
+        y = SitMap.POLE.selected,
+        scale = scale,
+        fun = ee$Reducer$mean(),
+        sf = FALSE
+    )
 }
+
+
+# ################################################################
+# # Worldclim/Bioclim 'WORLDCLIM/V1/BIO'
+# ################################################################
+
+wc <- ee$Image(gdl$worldclim$geeSnippet)
+
+bands_all <- wc$bandNames()$getInfo()
+# bands_all <- c("bio01", "bio02")
+
+
+
+# Extract values
+output_df[[paste0("wc_", scale, "_", season[1], "_stdev")]] <- ee_extract(
+    x = wc,
+    y = SitMap.POLE.selected,
+    scale = scale,
+    fun = ee$Reducer$stdDev(),
+    sf = FALSE
+)
+
+output_df[[paste0("wc_", scale, "_", season[1], "_mean")]] <- ee_extract(
+    x = wc,
+    y = SitMap.POLE.selected,
+    scale = scale,
+    fun = ee$Reducer$mean(),
+    sf = FALSE
+)
+
+
+
+# saveRDS(output_df, file = paste0(path.igaD, "test-kfme16-N_czechia_wc_l8_2018-2021_4-6.rds"))
+saveRDS(output_df, file = paste0(export_path, export_fileName, ".rds"))
+
 
 end_time <- Sys.time()
 
@@ -409,20 +402,15 @@ print(end_time - start_time)
 kfme16 <- output_df #  <- readRDS("/home/petr/Documents/igaD/predictorsDP/kfme0rad-all_czechia_wc_l8_2014-2017_4-8.rds")
 
 
-# sitmap_2rad$all <- st_transform(st_read(paste0(path.igaD, "sitmap_0rad/sitmap_0rad.shp")), crs=4326) %>% dplyr::select(POLE)
-sf.grid <- sitmap_2rad$all
+# # vzorový KFME raster pro 16 subkvadrátů
+# divBy <- 1 # 0rad: 1; 1rad: 2; 2rad: 4
 
-
-
-# vzorový KFME raster pro 16 subkvadrátů
-divBy <- 1 # 0rad: 1; 1rad: 2; 2rad: 4
-
-r <- raster(
-    ext = extent(sitmap_2rad$all),
-    res = c((1 / 6) / divBy, 0.1 / divBy), # resolution in c(x,y) direction
-    vals = rep(1, nrow(sitmap_2rad$all))
-)
-# writeRaster(r, paste0(path.igaD, "kfme16-test.tif"))
+# r <- raster(
+#     ext = extent(sitmap_2rad$all),
+#     res = c((1 / 6) / divBy, 0.1 / divBy), # resolution in c(x,y) direction
+#     vals = rep(1, nrow(sitmap_2rad$all))
+# )
+# # writeRaster(r, paste0(path.igaD, "kfme16-test.tif"))
 
 
 
@@ -465,7 +453,7 @@ for (l1 in names(kfme16)) {
 names(tbl) <- str_replace_all(names(tbl), c("_constant$" = "", "_nd$" = ""))
 sort(names(tbl))
 
-write.csv(tbl, paste0(export_path, export_fileName, "---", half, ".csv"), row.names = FALSE)
+write.csv(tbl, paste0(export_path, export_fileName, ".csv"), row.names = FALSE)
 
 print("celkem:")
 print(Sys.time() - start_time)
